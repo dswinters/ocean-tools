536b6595ea8c969d7c27bca3cbff9090e8106063
Dylan Winters <dylan@dw-mbp.local>
2017-03-13 11:59:34 -0700

    fix pitch/roll

--- adcp_beam2earth.m
+++ adcp_beam2earth.m
@@ -7,9 +7,6 @@
 % Created: Mar 12 2017
 
 function A = adcp_beam2earth(A)
-% clear all, close all
-% load('../Data/puget_2017_jan/Spanky/raw/ADCP/SPANKY_2017_01_18_2040.mat')
-
 
 % Check that velocities are in beam coordinates
 if ~strcmp(A.config.coord_sys,'beam')
@@ -17,6 +14,7 @@ if ~strcmp(A.config.coord_sys,'beam')
            ' (currently ''%s'')'],A.config.coord_sys)
 end
 
+%% Constants
 nbeams = A.config.n_beams;
 ncells = A.config.n_cells;
 isConvex = strcmp(A.config.beam_pattern,'convex');
@@ -25,10 +23,19 @@ hasBT = isfield(A,'bt_vel');
 cb = cosd(A.config.beam_angle);
 sb = sind(A.config.beam_angle);
 h0 = A.config.xducer_misalign;
-sp =@(t) sind(A.pitch(t));
-sr =@(t) sind(A.roll(t));
-cp =@(t) cosd(A.pitch(t));
-cr =@(t) cosd(A.roll(t));
+
+%% Functions for getting rotation constants
+% Internal tilt sensors don't directly measure pitch/roll
+st1 =@(t) sind(A.pitch(t)); % sin(tilt1)
+ct1 =@(t) cosd(A.pitch(t)); % cos(tilt1)
+st2 =@(t) sind(A.roll(t));  % sin(tilt2)
+ct2 =@(t) cosd(A.roll(t));  % cos(tilt2)
+% This is how we convert tilt1&tilt2 into pitch:
+getPitch =@(t) asind(st1(t)*ct2(t) / sqrt(1 - (st1(t)*st2(t))^2));
+sp =@(t) sind(getPitch(t)); % sin(pitch)
+cp =@(t) cosd(getPitch(t)); % cos(pitch)
+sr =@(t) sind(A.roll(t));   % sin(roll)
+cr =@(t) cosd(A.roll(t));   % cos(roll)
 
 %% Define some rotation functions
 % Add 4th dimension for error velocity
@@ -42,8 +49,8 @@ roty=@(d) [+cosd(d) 0 +sind(d) 0 ;
            -sind(d) 0 +cosd(d) 0 ;
            0        0        0 1];
 
-rotz=@(d) [+cosd(d) -sind(d) 0 0 ;
-           +sind(d) +cosd(d) 0 0 ;
+rotz=@(d) [+cosd(d) +sind(d) 0 0 ;
+           -sind(d) +cosd(d) 0 0 ;
                   0        0 1 0 ;
                   0        0 0 1];
 
@@ -58,21 +65,15 @@ b2i = [c*a -c*a  0     0  ;
        d    d    -d   -d  ];
 
 %% Earth coordinate transformation
-% From "adcp coordinate transformation" doc:
-% getPitch = atand(tand(A.pitch(t)).*cosd(A.roll(t))); 
-% From beam2earth_workhorse.m:
-getPitch =@(t) asind(sp(t)*cr(t) / sqrt(1 - (sp(t)*sr(t))^2));
-i2s =@(t) rotz(-h0) * rotx(getPitch(t)) * roty(A.roll(t));
-s2e =@(t) rotz(-A.heading(t));
-% i2e =@(t) rotz(A.heading(t) - h0) * rotx(getPitch(t)) * roty(A.roll(t));
+i2e =@(t) rotz(A.heading(t) + h0) * rotx(getPitch(t)) * roty(A.roll(t));
 
 %% Bin mapping
 % Beam depth scale factors (tilted->flat)
-os = 2*[isConvex; ~isConvex; isUp&isConvex; ~(isUp&isConvex)]-1;
+ori = 2*[isConvex; ~isConvex; isUp&isConvex; ~(isUp&isConvex)]-1;
 m12 = @(t) [-sr(t)*cp(t)*[1;1]; 
             sp(t)*[1;1]]; 
 m3  = @(t) cp(t).*cr(t);
-sd  = @(t) [cb ./ (m3(t).*cb + os.*m12(t).*sb)];
+sd  = @(t) [cb ./ (m3(t).*cb + ori.*m12(t).*sb)];
 
 % get bin numbers for each depth cell per beam
 binmap =@(t) fix(sd(t)*[1:ncells]+0.5);
@@ -101,17 +102,16 @@ for t = 1:length(A.mtime)
     vb = vb_bm(t,bm); % bin-mapped beam velocity
     vb(rmbin) = NaN; % remove invalid cells
 
-    % %% Apply 3-beam solutions where only 1 beam is bad:
-    % use_3beam = find(sum(isnan(vb))==1);
-    % badbeam = nbeams+1 - sum(cumsum(isnan(vb(:,use_3beam))));
-    % for i = 1:length(use_3beam)
-    %     vb(badbeam(i),use_3beam(i)) = ...
-    %         nansum(weights_3beam(badbeam(i)).*vb(:,use_3beam(i)));
-    % end
+    %% Apply 3-beam solutions where only 1 beam is bad:
+    use_3beam = find(sum(isnan(vb))==1);
+    badbeam = nbeams+1 - sum(cumsum(isnan(vb(:,use_3beam))));
+    for i = 1:length(use_3beam)
+        vb(badbeam(i),use_3beam(i)) = ...
+            nansum(weights_3beam(badbeam(i)).*vb(:,use_3beam(i)));
+    end
 
     %% Apply coordinate transformations
-    ve = s2e(t)*i2s(t)*b2i*vb;
-    % ve = i2e(t)*b2i*vb;
+    ve = i2e(t)*b2i*vb;
     East(:,t) = ve(1,:);
     North(:,t) = ve(2,:);
     Vert(:,t) = ve(3,:);
@@ -119,8 +119,7 @@ for t = 1:length(A.mtime)
 
     if hasBT
         A.bt_vel(:,t) = ...
-            1/1000*s2e(t)*i2s(t)*b2i*A.bt_vel(:,t);
-            % 1/1000*i2e(t)*b2i*A.bt_vel(:,t);
+            1/1000*i2e(t)*b2i*A.bt_vel(:,t);
     end
 end
 
