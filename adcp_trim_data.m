62ec30bb03db1405c62da819ef5945a51a9988a7
Dylan Winters <dylan@dw-mbp.local>
2017-03-24 11:25:30 -0700

    add beam-averaged correlation method

--- adcp_trim_data.m
+++ adcp_trim_data.m
@@ -24,10 +24,19 @@ bnames = {'east','north','vert','error'};
 for i = 1:length(trim_methods)
     mes = 'Data removed ';
     switch trim_methods(i).name
+      %% Do nothing
+      case 'none'
+        return
       %% Remove data below a percentage of bottom-track depth
       case 'BT'
+        % Make sure the BT parameter is given as a percentage
         pct = trim_methods(i).params;
-        mes = [mes 'below %.2f%% of bottom-track depth'];
+        if pct<1
+            warning(sprintf(['BT percentage parameter was given as %.2f. ',...
+                             'Assuming this means %.2f%%'],pct,100*pct));
+            pct=100*pct;
+        end
+        mes = [mes sprintf('below %.2f%% of bottom-track depth',pct)];
         for ib = 1:4;
             bd = adcp.bt_range(ib,:);
             bd(bd<5) = inf; % don't trust very shallow BT depths
@@ -37,13 +46,33 @@ for i = 1:length(trim_methods)
         adcp.info = cat(1,adcp.info,mes);
 
       %% Remove data using echo intensity edge-detection
-      case 'EI'
+      case 'ei_edge'
         mes = [mes 'using echo intensity edge detection'];
-        % look for sharp increases in echo intensity, remove data below
         for ib = 1:4
             edges = edge(squeeze(adcp.intens(:,ib,:)),'Sobel','horizontal');
             bt_mask = (cumsum(edges) > 0);
             adcp.([bnames{ib} '_vel'])(bt_mask) = NaN;
+        end
+        adcp.info = cat(1,adcp.info,mes);
+
+      %% Remove data using correlation edge-detection
+      case 'corr_edge'
+        switch trim_methods(i).params
+          case 'beam'
+            mes = [mes 'using beam-specific correlation edge detection'];
+            for ib = 1:4
+                edges = edge(squeeze(adcp.corr(:,ib,:)),'Sobel','horizontal');
+                bt_mask = (cumsum(edges) > 0);
+                adcp.([bnames{ib} '_vel'])(bt_mask) = NaN;
+            end
+            adcp.info = cat(1,adcp.info,mes);
+          case 'avg'
+            mes = [mes 'using beam-averaged correlation edge detection'];
+            edges = edge(squeeze(nanmean(adcp.corr,2)),'Sobel','horizontal');
+            bt_mask = (cumsum(edges) > 0);
+            for ib = 1:4
+                adcp.([bnames{ib} '_vel'])(bt_mask) = NaN;                        
+            end
         end
         adcp.info = cat(1,adcp.info,mes);
 
