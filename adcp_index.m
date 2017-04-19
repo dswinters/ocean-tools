385e791b386ecf28e24feebf301235bfaa8d6a3d
Dylan Winters <dylan@dw-mbp.local>
2017-04-19 16:23:48 -0700

    rewrite

--- adcp_index.m
+++ adcp_index.m
@@ -1,45 +1,23 @@
-%%% adcp_index.m --- 
-%% Usage: A = adcp_index(A,idx)
-%% Description: returns an ADCP data structure with only 
-%%%             the specified time indices
-%% Inputs: A - ADCP structure
-%% Outputs: A - ADCP structure with limited time indices
-%% 
-%% Author: Dylan Winters
-%% Created: August 10 2016
+%% adcp_index.m --- 
+% Usage: A = adcp_index(A,idx)
+% Description: returns an ADCP data structure with only 
+%              the specified time indices. This function
+%              assumes that the time dimension is last for 
+%              all fields in the ADCP data structure.
+% Inputs: A - ADCP structure
+% Outputs: A - ADCP structure with limited time indices
+% 
+% Author: Dylan Winters
+% Created: August 10 2016
 
 function A = adcp_index(A,idx)
 
-A.mtime        = A.mtime(:,idx);
-A.number       = A.number(:,idx);
-A.pitch        = A.pitch(:,idx);
-A.roll         = A.roll(:,idx);
-A.heading      = A.heading(:,idx);
-A.pitch_std    = A.pitch_std(:,idx);
-A.roll_std     = A.roll_std(:,idx);
-A.heading_std  = A.heading_std(:,idx);
-A.depth        = A.depth(:,idx);
-A.temperature  = A.temperature(:,idx);
-A.salinity     = A.salinity(:,idx);
-A.pressure     = A.pressure(:,idx);
-A.pressure_std = A.pressure_std(:,idx);
-A.east_vel     = A.east_vel(:,idx);
-A.north_vel    = A.north_vel(:,idx);
-A.vert_vel     = A.vert_vel(:,idx);
-A.error_vel    = A.error_vel(:,idx);
-A.corr         = A.corr(:,:,idx);
-A.status       = A.status(:,:,idx);
-A.intens       = A.intens(:,:,idx);
-A.bt_range     = A.bt_range(:,idx);
-A.bt_vel       = A.bt_vel(:,idx);
-A.bt_corr      = A.bt_corr(:,idx);
-A.bt_ampl      = A.bt_ampl(:,idx);
-A.bt_perc_good = A.bt_perc_good(:,idx);
-A.perc_good    = A.perc_good(:,:,idx);
-
-if isfield(A,'file')
-    A.file = A.file(:,idx);
-end
-if isfield(A,'mtime_raw')
-    A.mtime_raw = A.mtime_raw(:,idx);
+flds = setdiff(fields(A),{'config','files'});
+for i = 1:length(flds)
+    nd = ndims(A.(flds{i}));
+    if nd == 2 & ~isstr(A.(flds{i}));
+        A.(flds{i}) = A.(flds{i})(:,idx);
+    elseif nd == 3 & ~isstr(A.(flds{i}));
+        A.(flds{i}) = A.(flds{i})(:,:,idx);
+    end
 end
